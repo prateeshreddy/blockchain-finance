c13230291350e01cedd0998023d28e21c5088970
Prateesh Reddy <60734422+prateeshreddy@users.noreply.github.com>
2020-04-03 22:09:51 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAu8gTuEgur3t_R1s7Y8pMON0Miql-Pvxk',
+    authDomain: 'blockchain-finance.firebaseapp.com',
+    databaseURL: 'https://blockchain-finance.firebaseio.com',
+    projectId: 'blockchain-finance',
+    storageBucket: 'blockchain-finance.appspot.com',
+    messagingSenderId: '591050650236',
+    appId: '1:591050650236:web:b8b0ef785fc1de8cf68ff9'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAu8gTuEgur3t_R1s7Y8pMON0Miql-Pvxk',
+    authDomain: 'blockchain-finance.firebaseapp.com',
+    databaseURL: 'https://blockchain-finance.firebaseio.com',
+    projectId: 'blockchain-finance',
+    storageBucket: 'blockchain-finance.appspot.com',
+    messagingSenderId: '591050650236',
+    appId: '1:591050650236:web:b8b0ef785fc1de8cf68ff9'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
